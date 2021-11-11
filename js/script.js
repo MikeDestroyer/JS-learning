7a768c5b2dcecdc2a16b48b60824d703ae30357d
MikeDestroyer <Treil911@gmail.com>
2021-11-11 15:20:32 +0300

    Выполнил задание №3

--- js/script.js
+++ js/script.js
@@ -1,16 +1,44 @@
-const title = 'Калькулятор';
-let screens = 'Простые, Сложные, Интерактивные';
-const screenPrice = 82;
-const rollback = 32;
-const fullPrice = 300;
-let adaptive = true;
+'use strict';
 
-console.log('Тип переменной title -', typeof title);
-console.log('Тип переменной fullPrice -', typeof fullPrice);
-console.log('Тип переменной adaptive -', typeof adaptive);
-console.log('Длина строки переменной title -', title.length, 'символов');
-console.log('Стоимость верстки экранов', screenPrice, 'Доллара');
-console.log('Стоимость разработки сайта', fullPrice, 'Bucks');
-screens = screens.toLowerCase();
-console.log(screens.split(', '));
-console.log('Процент отката посреднику за работу ', fullPrice * (rollback / 100));
+const rollback = 3200;
+
+let title = prompt('Название проекта');
+console.log('Название проекта -', title);
+let screens = prompt('Какие типы экранов нужно разработать');
+console.log('типы экранов:', screens);
+let screenPrice = +prompt('Сколько будет стоить данная работа?');
+console.log('стоит данная работа', screenPrice);
+let adaptive = confirm('Нужен ли адаптив на сайте?');
+console.log('Нужен ли адаптив на сайте?', adaptive);
+
+let service1 = prompt('Какой первый дополнительный тип услуги нужен?');
+console.log('Какой первый дополнительный тип услуги нужен?', service1);
+let servicePrice1 = +prompt('Сколько это будет стоить?');
+console.log('Сколько это будет стоить?', servicePrice1);
+let service2 = prompt('Какой второй дополнительный тип услуги нужен?');
+console.log('Какой первый дополнительный тип услуги нужен?', service2);
+let servicePrice2 = +prompt('Сколько это будет стоить?');
+console.log('Сколько это будет стоить?', servicePrice2);
+
+
+let fullPrice = screenPrice + servicePrice1 + servicePrice2;
+console.log('Суммарная стоимость услуг:', fullPrice, 'рублей');
+let servicePercentPrice = fullPrice - rollback;
+console.log('Стоимость услуг с учётом отката:', servicePercentPrice, 'рублей');
+
+switch (true) {
+    case servicePercentPrice > 30000:
+        console.log('Скидка 10%');
+        console.log(servicePercentPrice / 100 * 10);
+        break;
+    case 30000 > servicePercentPrice && servicePercentPrice > 15000:
+        console.log('Скидка 5%');
+        console.log(servicePercentPrice / 100 * 5);
+        break;
+    case 15000 > servicePercentPrice && servicePercentPrice > 0:
+        console.log('Скидка не предусмотрена');
+        console.log(servicePercentPrice);
+        break;
+    case 0 > servicePercentPrice:
+        console.log('Что то пошло не так');
+}
