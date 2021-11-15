172b39447f69595dc9e45316aa057c7f6b519cca
MikeDestroyer <Treil911@gmail.com>
2021-11-15 15:56:57 +0300

    Выполнил задание №4

--- js/script.js
+++ js/script.js
@@ -3,51 +3,46 @@
 const rollback = 3200;
 
 let title = prompt('Название проекта');
-// let screens = prompt('Какие типы экранов нужно разработать');
+let screens = prompt('Какие типы экранов нужно разработать');
 let screenPrice = +prompt('Сколько будет стоить данная работа?');
 // let adaptive = confirm('Нужен ли адаптив на сайте?');
 // let service1 = prompt('Какой первый дополнительный тип услуги нужен?');
 let servicePrice1 = +prompt('Сколько это будет стоить?');
 // let service2 = prompt('Какой второй дополнительный тип услуги нужен?');
 let servicePrice2 = +prompt('Сколько это будет стоить?');
-let allServicePrices, servicePercentPrice, fullPrice;
+let allServicePrices, servicePercentPrices, fullPrice;
 
 const getAllServicePrices = function () {
-
     return servicePrice1 + servicePrice2;
 };
-function getFullPrice() {
-    return screenPrice + allServicePrices;
-}
 function getTitle() {
     return title.trim().charAt(0).toUpperCase() + title.trim().toLowerCase().slice(1);
 }
-let getServicePercentPrices = function () {
+function getFullPrice() {
+    return screenPrice + allServicePrices;
+}
+function getServicePercentPrices() {
     return fullPrice - rollback;
-};
+}
 function getRollbackMessage() {
-    if (servicePercentPrice > 30000) {
+    if (servicePercentPrices > 30000) {
         return 'Скидка 10%';
-    } else if (servicePercentPrice > 15000) {
+    } else if (servicePercentPrices > 15000) {
         return 'Скидка 5%';
-    } else if (servicePercentPrice > 0) {
+    } else if (servicePercentPrices > 0) {
         return 'Скидка не предусмотрена';
     } else {
         return 'Что то пошло не так';
     }
 }
+
+title = getTitle();
+screens = screens.split(', ');
 allServicePrices = getAllServicePrices();
 fullPrice = getFullPrice();
-servicePercentPrice = getServicePercentPrices();
-title = getTitle();
+servicePercentPrices = getServicePercentPrices();
 
-console.log('Название проекта -', title);
-// console.log('типы экранов:', screens);
-// console.log('стоит данная работа', screenPrice);
-// console.log('Нужен ли адаптив на сайте?', adaptive);
-// console.log('Какой первый дополнительный тип услуги нужен?', service1);
-// console.log('Сколько это будет стоить?', servicePrice1);
-// console.log('Какой первый дополнительный тип услуги нужен?', service2);
-// console.log('Сколько это будет стоить?', servicePrice2);
-// console.log('Суммарная стоимость услуг:', fullPrice, 'рублей');
-// console.log('Стоимость услуг с учётом отката:', servicePercentPrice, 'рублей');
+console.log('Название проекта:', title);
+console.log('Типы экранов:', typeof (screens), screens);
+console.log('Стоимость за вычетом процента отката посреднику: ', getServicePercentPrices());
+console.log('Бонус для пользователя: ', getRollbackMessage());
