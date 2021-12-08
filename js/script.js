6de79e3dc63eab4043b167d96b5a40274534e925
MikeDestroyer <Treil911@gmail.com>
2021-12-09 00:58:18 +0300

    discharge changes

--- js/script.js
+++ js/script.js
@@ -1,65 +1,81 @@
 'use strict';
 
+const rollback = 3200;
+let title, screens, adaptive, screenPrice, allServicePrices, servicePercentPrices, fullPrice, service2, service1;
+
 function isNumber(num) {
     return !isNaN(parseFloat(num)) && isFinite(num);
 }
 
-function GetRandomInt() {
-    return Math.floor(Math.random() * 101);
+function asking() {
+    title = prompt('Название проекта');
+    screens = prompt('Какие типы экранов нужно разработать');
+
+    let num;
+    do {
+        num = prompt('Сколько будет стоить данная работа?');
+        console.log(typeof (num), num);
+    } while (!isNumber(num));
+    screenPrice = +num;
+
+    adaptive = confirm('Нужен ли адаптив на сайте?');
 }
 
-function nubmerComparsion(num, rand) {
-    if (num > rand) {
-        alert('Загаданное число больше');
-        return false;
-    } else if (num < rand) {
-        alert('Загаданное число меньше');
-        return false;
-    } else if (num === rand) {
-        return true;
-    }
-}
+const getAllServicePrices = function () {
+    let count = 0;
 
-function GuessTheNumberGame() {
-    let rand = GetRandomInt();
-    let userNum = 0;
-    let num, restart;
-    console.log(typeof (rand), rand);
+    for (let i = 0; i < 2; i++) {
+        let num = 0;
 
-    num = prompt('Супер игра! Угадай число от 0 до 100 и получить годовой запас НИХУЯ!');
-    if (num == null) {
-        return 'Слабак';
-    }
-    if (!isNumber(num)) {
-        do {
-            num = prompt('Ссука, введи число!');
-        } while (isNumber(num) === false && num !== null);
-    }
-    if
-        (num == null) {
-        return 'Слабак';
-    }
-    userNum = +num;
-    if (userNum === null) {
-        return 'Слабак';
-    }
-
-
-    if (nubmerComparsion(userNum, rand)) {
-        return 'Поздравляю, сегодня твой счастливый день! Собирай вещички и езжай прямиком в Вегас!';
-    } else {
-        restart = confirm('Хотите сыграть ещё раз?');
-        if (restart == true) {
-            return GuessTheNumberGame();
+        if (i === 0) {
+            service1 = prompt('Какой первый дополнительный тип услуги нужен?');
         } else {
-            return 'Слабак';
+            service2 = prompt('Какой второй дополнительный тип услуги нужен?');
         }
+
+        do {
+            num = prompt('Сколько это будет стоить?');
+            console.log(num);
+        } while (!isNumber(num));
+
+        count += +num;
+    }
+    return count;
+};
+
+function getTitle() {
+    return title.trim().charAt(0).toUpperCase() + title.trim().toLowerCase().slice(1);
+}
+
+function getFullPrice() {
+    return screenPrice + allServicePrices;
+}
+
+function getServicePercentPrices() {
+    return fullPrice - rollback;
+}
+
+function getRollbackMessage() {
+    if (servicePercentPrices > 30000) {
+        return 'Скидка 10%';
+    } else if (servicePercentPrices > 15000) {
+        return 'Скидка 5%';
+    } else if (servicePercentPrices > 0) {
+        return 'Скидка не предусмотрена';
+    } else {
+        return 'Что то пошло не так';
     }
 }
 
+asking();
+title = getTitle();
+screens = screens.split(', ');
+allServicePrices = getAllServicePrices();
+fullPrice = getFullPrice();
+servicePercentPrices = getServicePercentPrices();
 
-alert(GuessTheNumberGame());
 
-
-
-
+console.log('Название проекта:', title);
+console.log('Типы экранов:', typeof (screens), screens);
+console.log('Стоимость за вычетом процента отката посреднику: ', getServicePercentPrices());
+console.log('Бонус для пользователя: ', getRollbackMessage());
