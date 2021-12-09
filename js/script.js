3164c9f57370cd1e7bdee4d537949b96526c15ef
MikeDestroyer <Treil911@gmail.com>
2021-12-09 03:39:46 +0300

    Я уверовал в Аллаха №8

--- js/script.js
+++ js/script.js
@@ -4,19 +4,26 @@
 const appData = {
 
     title: '',
-    screens: '',
+    screens: [],
     adaptive: true,
     screenPrice: 0,
     rollback: 3200,
     allServicePrices: 0,
     servicePercentPrices: 0,
     fullPrice: 0,
-    service2: '',
-    service1: '',
+    services: {},
+
+    start: function () {
+        appData.asking();
+        appData.getTitle();
+        appData.getPrice();
+        appData.getFullPrice();
+        appData.getServicePercentPrices();
+        appData.logger();
+    },
 
     asking: function () {
         appData.title = prompt('Название проекта');
-        appData.screens = prompt('Какие типы экранов нужно разработать');
 
         let num;
         do {
@@ -26,44 +33,50 @@ const appData = {
         appData.screenPrice = +num;
 
         appData.adaptive = confirm('Нужен ли адаптив на сайте?');
+
+        for (let i = 0; i < 2; i++) {
+            let price = 0;
+            let name = prompt('Какой дополнительный тип услуги нужен?');
+
+            do {
+                price = prompt('Сколько это будет стоить?');
+            } while (!appData.isNumber(price));
+            appData.services[name + i] = +price;
+        }
+
+        for (let i = 0; i < 2; i++) {
+            let price = 0;
+            let name = prompt('Какие типы экранов нужно разработать');
+            do {
+                price = prompt('Сколько это будет стоить?');
+            } while (!appData.isNumber(price));
+            appData.screens.push({ id: i, name: name, price: price });
+        }
     },
 
     isNumber: function (num) {
         return !isNaN(parseFloat(num)) && isFinite(num);
     },
-
-    getAllServicePrices: function () {
-        let count = 0;
-
-        for (let i = 0; i < 2; i++) {
-            let num = 0;
-
-            if (i === 0) {
-                appData.service1 = prompt('Какой первый дополнительный тип услуги нужен?');
-            } else {
-                appData.service2 = prompt('Какой второй дополнительный тип услуги нужен?');
-            }
-
-            do {
-                num = prompt('Сколько это будет стоить?');
-                console.log(num);
-            } while (!appData.isNumber(num));
-
-            count += +num;
+    getPrice: function () {
+        appData.allServicePrices = appData.screens.reduce(function (sum, appData) {
+            return sum + appData.screens.price;
+        }, 0);
+        for (let key in appData.services) {
+            appData.allServicePrices += appData.services[key];
         }
-        return count;
+
     },
 
     getTitle: function () {
-        return appData.title.trim().charAt(0).toUpperCase() + appData.title.trim().toLowerCase().slice(1);
+        appData.title = appData.title.trim().charAt(0).toUpperCase() + appData.title.trim().toLowerCase().slice(1);
     },
 
     getFullPrice: function () {
-        return appData.screenPrice + appData.allServicePrices;
+        appData.fullPrice = appData.screenPrice + appData.allServicePrices;
     },
 
     getServicePercentPrices: function () {
-        return appData.fullPrice - appData.rollback;
+        appData.servicePercentPrices = appData.fullPrice - appData.rollback;
     },
 
     getRollbackMessage: function () {
@@ -78,24 +91,18 @@ const appData = {
         }
     },
 
-    start: function () {
-        appData.asking();
-        appData.title = appData.getTitle();
-        appData.screens = appData.screens.split(', ');
-        appData.allServicePrices = appData.getAllServicePrices();
-        appData.fullPrice = appData.getFullPrice();
-        appData.servicePercentPrices = appData.getServicePercentPrices();
-        appData.logger();
-    },
-
     logger: function () {
         console.log('Название проекта:', appData.title);
         console.log('Типы экранов:', typeof (screens), appData.screens);
-        console.log('Стоимость за вычетом процента отката посреднику: ', appData.getServicePercentPrices());
+        console.log('Стоимость за вычетом процента отката посреднику: ', appData.servicePercentPrices);
         console.log('Бонус для пользователя: ', appData.getRollbackMessage());
-        for (let key in appData) {
-            console.log('Имя свойства: ' + key);
+
+        for (let key in appData.services) {
+            console.log('Имя свойства: ' + key + " Значение: " + appData.services[key]);
         }
+
+        console.log(appData.services);
+        console.log(appData.screens);
     }
 };
 
