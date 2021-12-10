5bfbbedb414571ce02feb974c86818ca6a2d6be5
MikeDestroyer <Treil911@gmail.com>
2021-12-10 03:11:59 +0300

    Не сделал задание №9

--- js/script.js
+++ js/script.js
@@ -1,121 +1,41 @@
 'use strict';
 
+const title = getElementByTagFromArray('h1', 0);
+const reset = getElementByClassFromArray('handler_btn', 0);
+const start = getElementByClassFromArray('handler_btn', 1);
+const addScreen = document.querySelector('.screen-btn');
+const otherItems1 = document.querySelectorAll('.other-items', '.number');
+const otherItems2 = document.querySelectorAll('.other-items', '.percent');
+const rollback = document.querySelector('.rollback > div > input');
+const span = document.querySelector('.rollback > div > .range-value');
 
-const appData = {
 
-    title: '',
-    screens: [],
-    adaptive: true,
-    screenPrice: 0,
-    rollback: 3200,
-    allServicePrices: 0,
-    servicePercentPrices: 0,
-    fullPrice: 0,
-    services: {},
+function getElementByTagFromArray(tagName, index) {
+    let tag = document.getElementsByTagName(tagName);
+    return tag[index];
+}
 
-    start: function () {
-        appData.asking();
-        appData.getTitle();
-        appData.getPrice();
-        appData.getFullPrice();
-        appData.getServicePercentPrices();
-        appData.logger();
-    },
+function getElementByClassFromArray(className, index) {
+    let tag = document.getElementsByClassName(className);
+    return tag[index];
+}
 
-    asking: function () {
-        appData.title = prompt('Название проекта');
-
-        let num;
-        do {
-            num = prompt('Сколько будет стоить данная работа?');
-            console.log(typeof (num), num);
-        } while (!appData.isNumber(num));
-        appData.screenPrice = +num;
-
-        appData.adaptive = confirm('Нужен ли адаптив на сайте?');
-
-        for (let i = 0; i < 2; i++) {
-            let price = 0;
-            let name = prompt('Какой дополнительный тип услуги нужен?');
-
-            do {
-                price = prompt('Сколько это будет стоить?');
-            } while (!appData.isNumber(price));
-            appData.services[name + i] = +price;
-        }
-
-        for (let i = 0; i < 2; i++) {
-            let price = 0;
-            let name = prompt('Какие типы экранов нужно разработать');
-            do {
-                price = prompt('Сколько это будет стоить?');
-            } while (!appData.isNumber(price));
-            appData.screens.push({ id: i, name: name, price: price });
-        }
-    },
-
-    isNumber: function (num) {
-        return !isNaN(parseFloat(num)) && isFinite(num);
-    },
-    getPrice: function () {
-        appData.allServicePrices = appData.screens.reduce(function (sum, elem) {
-            return sum + +elem.price;
-        }, 0);
-        for (let key in appData.services) {
-            appData.allServicePrices += appData.services[key];
-        }
-
-    },
-
-    getTitle: function () {
-        appData.title = appData.title.trim().charAt(0).toUpperCase() + appData.title.trim().toLowerCase().slice(1);
-    },
-
-    getFullPrice: function () {
-        appData.fullPrice = appData.screenPrice + appData.allServicePrices;
-    },
-
-    getServicePercentPrices: function () {
-        appData.servicePercentPrices = appData.fullPrice - appData.rollback;
-    },
-
-    getRollbackMessage: function () {
-        if (appData.servicePercentPrices > 30000) {
-            return 'Скидка 10%';
-        } else if (appData.servicePercentPrices > 15000) {
-            return 'Скидка 5%';
-        } else if (appData.servicePercentPrices > 0) {
-            return 'Скидка не предусмотрена';
-        } else {
-            return 'Что то пошло не так';
-        }
-    },
-
-    logger: function () {
-        console.log('Название проекта:', appData.title);
-        console.log('Типы экранов:', typeof (screens), appData.screens);
-        console.log('Стоимость за вычетом процента отката посреднику: ', appData.servicePercentPrices);
-        console.log('Бонус для пользователя: ', appData.getRollbackMessage());
-
-        for (let key in appData.services) {
-            console.log('Имя свойства: ' + key + " Значение: " + appData.services[key]);
-        }
-
-        console.log(appData.services);
-        console.log(appData.screens);
+function getVarsFromClass(ClassName, varName) {
+    let lenght = document.getElementsByClassName(ClassName).length;
+    // console.log(lenght);
+    for (let i = 0; i < lenght; i++) {
+        let(varName + i) = ;
     }
-};
 
-appData.start();
-
-
-
-
-
-
-
-
-
-
-
-
+}
+getVarsFromClass('total-input');
+console.log(rollback);
+console.log(span);
+// // rollback = rollback.getAttribute()
+// console.log(title);
+// console.log(start);
+// console.log(reset);
+// console.log(addScreen);
+// console.log(otherItems1);
+// console.log(otherItems2);
+// console.log(rollback);
