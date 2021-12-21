ba8717d5b407a8b8b800fbf3c3c9691f6431a4bd
MikeDestroyer <Treil911@gmail.com>
2021-12-21 18:10:39 +0300

    Не работает п1 в задании

--- js/script.js
+++ js/script.js
@@ -7,13 +7,16 @@ const appData = {
     otherItemsNumber: document.querySelectorAll('.other-items.number'),
     otherItemsPercent: document.querySelectorAll('.other-items.percent'),
     totalInputs: document.querySelectorAll('.total-input'),
+    rangeRollback: document.querySelector('input[type=range]'),
+    rangeValue: document.querySelector('.range-value'),
 
 
     title: '',
     screens: [],
     adaptive: true,
     screenPrice: 0,
-    rollback: 3200,
+    rollback: 0,
+    rollbackPercent: 0,
     servicePricesPercent: 0,
     servicePricesNumber: 0,
     servicePercentPrices: 0,
@@ -25,12 +28,42 @@ const appData = {
         appData.addTitle();
         appData.btnStart.addEventListener('click', appData.start);
         appData.btnPlus.addEventListener('click', appData.addScreenBlock);
+        appData.rangeRollback.addEventListener('input', appData.rangeInput);
+        // appData.startDisable();
+        appData.btnStart.disabled = true;
+
     },
 
     addTitle: function () {
         appData.title = document.getElementById('title');
         document.title = appData.title.textContent;
     },
+    rangeInput: function () {
+        console.log(appData.rangeValue.value);
+        appData.rollbackPercent = +appData.rangeRollback.value;
+        appData.rangeValue.textContent = appData.rangeRollback.value + '%';
+    },
+
+    // startDisable: function () {
+    //     appData.screen.forEach(function (screen, index) {
+    //         const select = screen.querySelector('select');
+    //         let input = screen.querySelector('input');
+    //         input.addEventListener('input', function () {
+    //             console.log(input.value);
+    //             // input.forEach(function () {
+    //             //     if (input.value !== '') {
+    //             //         appData.btnStart.disabled = true;
+    //             //         console.log('unlocked', input.value == '');
+    //             //         console.log(input.value);
+    //             //     } else {
+    //             //         console.log('locked');
+    //             //         appData.btnStart.disabled = false;
+    //             //     }
+    //             // });
+    //         });
+
+    //     });
+    // },
 
     start: function () {
         console.log('start');
@@ -58,6 +91,7 @@ const appData = {
     },
 
     addScreenBlock: function () {
+        console.log('testsets');
         const cloneScreen = appData.screen[0].cloneNode(true);
         appData.screen[appData.screen.length - 1].after(cloneScreen);
     },
@@ -94,6 +128,7 @@ const appData = {
         appData.servicePricesNumber = 0;
         appData.servicePricesPercent = 0;
         appData.fullPrice = 0;
+        appData.rollback = 0;
 
         for (let key in appData.screens) {
             appData.screenPrice += appData.screens[key].price;
@@ -106,6 +141,7 @@ const appData = {
         }
 
         appData.fullPrice = appData.screenPrice + appData.servicePricesNumber + appData.servicePricesPercent;
+        appData.rollback = appData.fullPrice * (appData.rollbackPercent / 100);
     },
     showResult: function () {
         let getScreenNumber = function () {
@@ -117,25 +153,18 @@ const appData = {
             console.log('sum ', result);
             return result;
         };
+
         appData.totalInputs[0].value = appData.screenPrice;
         appData.totalInputs[1].value = getScreenNumber();
-        console.log(appData.totalInputs[1].value);
         appData.totalInputs[2].value = appData.servicePricesNumber + appData.servicePricesPercent;
         appData.totalInputs[3].value = appData.fullPrice;
-        console.log(appData.screens[0].number);
+        appData.totalInputs[4].value = appData.rollback;
     }
 
 
 };
 
 appData.init();
-
-
-
-
-
-
-
 
 
 
