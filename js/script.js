0eb2ca861d732cc16909169d8f0d282b1670709a
MikeDestroyer <Treil911@gmail.com>
2021-12-24 02:09:07 +0300

    added consts

--- js/script.js
+++ js/script.js
@@ -1,6 +1,47 @@
 'use strict';
 
-let btnAdd = document.getElementById('add');
-let
+const btnAdd = document.getElementById('add')
+const headerInput = document.querySelector('.header-input')
+const toDoControl = document.querySelector('.todo-control')
+const toDoList = document.querySelector('.todo-list')
+const toDoCompeted = document.querySelector('.todo-completed')
+const isError = true
+
+const toDoData = [
+    {
+        text: 'some text',
+        completed: false
+    },
+    {
+        text: 'some more text',
+        completed: false
+    },
+    {
+        text: 'some even more text',
+        completed: false
+    },
+]
 
 
+toDoControl.addEventListener('submit', function (event) {
+    event.preventDefault()
+
+    const newToDo = {
+        text: headerInput.value,
+        completed: false
+    }
+
+    if (newToDo.trim() == '') {
+        // alert('Введите данные')
+        console.log('true');
+    } else {
+        console.log('else');
+        // toDoData.push(newToDo)
+    }
+
+    // render()
+})
+
+// const render = function () {
+//     if
+// }
