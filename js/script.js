5ea83a7714e4032219b41b372cd1d6fad8c025d8
MikeDestroyer <Treil911@gmail.com>
2021-12-24 04:15:43 +0300

    Выполнил задание №13

--- js/script.js
+++ js/script.js
@@ -7,41 +7,59 @@ const toDoList = document.querySelector('.todo-list')
 const toDoCompeted = document.querySelector('.todo-completed')
 const isError = true
 
-const toDoData = [
-    {
-        text: 'some text',
-        completed: false
-    },
-    {
-        text: 'some more text',
-        completed: false
-    },
-    {
-        text: 'some even more text',
-        completed: false
-    },
-]
+let toDoData = JSON.parse(localStorage.getItem('toDoData') || '[]')
 
 
 toDoControl.addEventListener('submit', function (event) {
     event.preventDefault()
-
     const newToDo = {
         text: headerInput.value,
         completed: false
     }
 
-    if (newToDo.trim() == '') {
-        // alert('Введите данные')
-        console.log('true');
+    if (newToDo.text.trim() == '') {
+        alert('Введите данные')
     } else {
-        console.log('else');
-        // toDoData.push(newToDo)
+        toDoData.push(newToDo)
+        headerInput.value = ''
+        render()
     }
 
-    // render()
 })
 
-// const render = function () {
-//     if
-// }
+const render = function () {
+    console.log(toDoData);
+    localStorage.removeItem(toDoData)
+    toDoList.innerHTML = ''
+    toDoCompeted.innerHTML = ''
+    toDoData.forEach(function (item, index) {
+        const li = document.createElement('li')
+        li.classList.add('todo-item')
+        li.innerHTML =
+            '<span class="text-todo">' + item.text + '</span>' +
+            '<div class="todo-buttons">' +
+            '<button class="todo-remove"></button>' +
+            '<button class="todo-complete"></button>' +
+            '</div>'
+
+        if (item.completed) {
+            toDoCompeted.append(li)
+        } else {
+            toDoList.append(li)
+        }
+        li.querySelector('.todo-remove').addEventListener('click', function () {
+            li.remove()
+            console.log(item)
+            toDoData.splice(index, 1)
+            render()
+        })
+        li.querySelector('.todo-complete').addEventListener('click', function () {
+            item.completed = !item.completed
+            render()
+        })
+    })
+
+    localStorage.setItem('toDoData', JSON.stringify(toDoData))
+}
+
+render()
