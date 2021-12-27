227304218e5dab43fbe26f86c9b5a9ca32d1a568
MikeDestroyer <Treil911@gmail.com>
2021-12-27 15:56:48 +0300

    update

--- js/script.js
+++ js/script.js
@@ -3,15 +3,22 @@
 const DomElement = function (selector, height, width, bg, fontSize) {
     let newElement
     this.selector = () => {
-        if (selector[0] == '.') {
-            newElement = 
+        if (selector.charAt(0) === '.') {
+            newElement = document.createElement('div')
+            newElement.classList.add(selector.substring(1))
+        } else if (selector.charAt(0) === '#') {
+            newElement = document.createElement('p')
+            newElement.setAttribute('id', selector.substring(1));
         }
     }
+    newElement.style.height = height
+    newElement.style.width = width
+    newElement.style.background = bg
+    newElement.style.fontSize = fontSize
+    newElement.appendChild(document.body)
+}
 
-
-
-    this.height = height
-    this.width = width
-    this.bg = bg
-    this.fontSize = fontSize
+function asking() {
+    alert('Введите данные для создания элемента HTML')
+    asking()
 }
