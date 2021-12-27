de901664bd2c9986ad259b97fbbce87a34c971cc
MikeDestroyer <Treil911@gmail.com>
2021-12-28 00:45:38 +0300

    another upload

--- js/script.js
+++ js/script.js
@@ -1,48 +1,41 @@
 'use strict'
 
 const DomElement = function (selector, height, width, bg, fontSize, text) {
-    let newElement
-    console.log(selector);
-    const element = () => {
-        console.log('start');
-        if (selector.charAt(0) === '.') {
-            console.log('true');
-            newElement = document.createElement('div')
-            newElement.classList.add(selector.substring(1))
-        } else if (selector.charAt(0) === '#') {
-            console.log('false');
-            newElement = document.createElement('p')
-            newElement.setAttribute('id', selector.substring(1));
-        } else {
-            console.log('bullshit');
-        }
+    let htmlElement
+    if (selector.charAt(0) === '.') {
+        htmlElement = document.createElement('div')
+        htmlElement.classList.add(selector.substring(1))
+    } else if (selector.charAt(0) === '#') {
+        htmlElement = document.createElement('p')
+        htmlElement.setAttribute('id', selector.substring(1));
+    } else {
     }
-    element()
-    console.log(newElement);
-    console.dir(newElement);
-    newElement.style.height = height
-    newElement.style.width = width
-    newElement.style.background = bg
-    newElement.style.fontSize = fontSize
-    newElement.textContent = text
-    newElement.appendChild(document.body)
+    htmlElement.style.height = height
+    htmlElement.style.width = width
+    htmlElement.style.background = bg
+    htmlElement.style.fontSize = fontSize
+    htmlElement.textContent = text
+    console.log(htmlElement);
+    this.exit = htmlElement
 }
 
-function asking() {
-    // alert('Введите данные для создания элемента HTML')
-    // const selector = prompt('введите id "#" или class "."')
-    // const height = prompt('Введите параметр css "height"')
-    // const width = prompt('Введите параметр css "width"')
-    // const bg = prompt('Введите параметр css "background"')
-    // const fontSize = prompt('Введите параметр css "fontSize"')
-    // const text = prompt('Введите текст для элемента')
-    const selector = '.one'
-    const height = '20px'
-    const width = '40px'
-    const bg = 'green'
-    const fontSize = '16px'
-    const text = 'hello world'
-    window.newElement = new DomElement(selector, height, width, bg, fontSize, text)
-}
 
-asking()
+// alert('Введите данные для создания элемента HTML')
+// const selector = prompt('введите id "#" или class "."')
+// const height = prompt('Введите параметр css "height"')
+// const width = prompt('Введите параметр css "width"')
+// const bg = prompt('Введите параметр css "background"')
+// const fontSize = prompt('Введите параметр css "fontSize"')
+// const text = prompt('Введите текст для элемента')
+const selector = '.one'
+const height = '20px'
+const width = '40px'
+const bg = 'green'
+const fontSize = '16px'
+const text = 'hello world'
+const element = new DomElement(selector, height, width, bg, fontSize, text)
+
+console.log(element);
+document.body.appendChild(element.exit)
+// element.exit.append()
+console.log(document.body);
