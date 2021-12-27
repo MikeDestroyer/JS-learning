0be20f84a619836608c92a9b758e0f5370ab4c79
MikeDestroyer <Treil911@gmail.com>
2021-12-27 15:15:23 +0300

    uploaded app

--- js/script.js
+++ js/script.js
@@ -1,63 +1,17 @@
-'use strict';
+'use strict'
 
-const btnAdd = document.getElementById('add')
-const headerInput = document.querySelector('.header-input')
-const toDoControl = document.querySelector('.todo-control')
-const toDoList = document.querySelector('.todo-list')
-const toDoCompeted = document.querySelector('.todo-completed')
-const isError = true
-
-let toDoData = JSON.parse(localStorage.getItem('toDoData') || '[]')
-
-
-toDoControl.addEventListener('submit', function (event) {
-    event.preventDefault()
-    const newToDo = {
-        text: headerInput.value,
-        completed: false
-    }
-
-    if (newToDo.text.trim() == '') {
-        alert('Введите данные')
-    } else {
-        toDoData.push(newToDo)
-        headerInput.value = ''
-        render()
-    }
-
-})
-
-const render = function () {
-    localStorage.removeItem(toDoData)
-    toDoList.innerHTML = ''
-    toDoCompeted.innerHTML = ''
-    toDoData.forEach(function (item, index) {
-        const li = document.createElement('li')
-        li.classList.add('todo-item')
-        li.innerHTML =
-            '<span class="text-todo">' + item.text + '</span>' +
-            '<div class="todo-buttons">' +
-            '<button class="todo-remove"></button>' +
-            '<button class="todo-complete"></button>' +
-            '</div>'
-
-        if (item.completed) {
-            toDoCompeted.append(li)
-        } else {
-            toDoList.append(li)
+const DomElement = function (selector, height, width, bg, fontSize) {
+    let newElement
+    this.selector = () => {
+        if (selector[0] == '.') {
+            newElement = 
         }
-        li.querySelector('.todo-remove').addEventListener('click', function () {
-            li.remove()
-            toDoData.splice(index, 1)
-            render()
-        })
-        li.querySelector('.todo-complete').addEventListener('click', function () {
-            item.completed = !item.completed
-            render()
-        })
-    })
+    }
 
-    localStorage.setItem('toDoData', JSON.stringify(toDoData))
+
+
+    this.height = height
+    this.width = width
+    this.bg = bg
+    this.fontSize = fontSize
 }
-
-render()
