2370aa84ac12a016cc60e585d724c67049ad85b3
MikeDestroyer <Treil911@gmail.com>
2021-12-28 16:39:16 +0300

    fixed lesson №15

--- js/script.js
+++ js/script.js
@@ -1,38 +1,49 @@
 'use strict'
 
-const DomElement = function (selector, height, width, bg, fontSize, text) {
-    let newElement
-    if (selector.charAt(0) === '.') {
-        console.log('true');
-        newElement = document.createElement('div')
-        newElement.classList.add(selector.substring(1))
-    } else if (selector.charAt(0) === '#') {
+const DomElement = function (selector, height, width, bg, fontSize, text, element) {
+    this.selector = selector
+    this.height = height
+    this.width = width
+    this.bg = bg
+    this.fontSize = fontSize
+    this.text = text
+    console.log(selector);
+    console.log(height);
 
-        newElement = document.createElement('p')
-        newElement.setAttribute('id', selector.substring(1));
-    }
-    newElement.style.height = height
-    newElement.style.width = width
-    newElement.style.background = bg
-    newElement.style.fontSize = fontSize
-    newElement.textContent = text
-    document.body.appendChild(newElement)
 }
 
+DomElement.prototype.displayElement = function () {
+    if (selector.charAt(0) === '.') {
+        console.log('true');
+        this.element = document.createElement('div')
+        this.element.classList.add(selector.substring(1))
+    } else if (selector.charAt(0) === '#') {
+        console.log('false');
+        this.element = document.createElement('p')
+        this.element.setAttribute('id', selector.substring(1));
+    }
+    this.element.style.height = this.height
+    this.element.style.width = this.width
+    this.element.style.background = this.bg
+    this.element.style.fontSize = this.fontSize
+    this.element.textContent = this.text
+    document.body.appendChild(this.element)
+}
 
-alert('Введите данные для создания элемента HTML')
-const selector = prompt('введите id "#" или class "."')
-const height = prompt('Введите параметр css "height"')
-const width = prompt('Введите параметр css "width"')
-const bg = prompt('Введите параметр css "background"')
-const fontSize = prompt('Введите параметр css "fontSize"')
-const text = prompt('Введите текст для элемента')
-// const selector = '.one'
-// const height = '200px'
-// const width = '400px'
-// const bg = 'green'
-// const fontSize = '16px'
-// const text = 'hello world'
+// alert('Введите данные для создания элемента HTML')
+// const selector = prompt('введите id "#" или class "."')
+// const height = prompt('Введите параметр css "height"')
+// const width = prompt('Введите параметр css "width"')
+// const bg = prompt('Введите параметр css "background"')
+// const fontSize = prompt('Введите параметр css "fontSize"')
+// const text = prompt('Введите текст для элемента')
+const selector = '.one'
+const height = '200px'
+const width = '400px'
+const bg = 'green'
+const fontSize = '16px'
+const text = 'hello world'
 const creacteElement = new DomElement(selector, height, width, bg, fontSize, text)
 console.log(creacteElement);
 
+creacteElement.displayElement()
