0120f3cac6aa8d29c4a388a97b6655964b531ff4
MikeDestroyer <Treil911@gmail.com>
2022-01-04 14:54:04 +0300

    completed task #3

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,33 +1,50 @@
 'use strict'
-let opacity = 0
-let posY = -5
 const modal = () => {
-
     const popup = document.querySelector('.popup')
     const popupWindow = document.querySelector('.popup-content')
     const buttons = document.querySelectorAll('.popup-btn')
     const btnPopupClose = document.querySelector('.popup-close')
+    let screenWidth
+    let opacity = 0
+    let posY = -5
+
+
+
+
 
     const animationPopupOpen = () => {
-        popup.style.display = 'block'
-        opacity = opacity + 0.02
-        posY = posY + 1
+        screenWidth = document.documentElement.clientWidth
+        console.log(screenWidth);
+        if (screenWidth >= 768) {
+            popup.style.display = 'block'
+            opacity = opacity + 0.05
+            posY = posY + 1
 
-        popup.style.backgroundColor = `rgba(0,0,0,${opacity})`
-        popupWindow.style.transform = `translateY(${posY}%)`
-        if (opacity < 0.5) {
-            requestAnimationFrame(animationPopupOpen)
+            popup.style.backgroundColor = `rgba(0,0,0,${opacity})`
+            popupWindow.style.transform = `translateY(${posY}%)`
+            if (opacity < 0.5) {
+                requestAnimationFrame(animationPopupOpen)
+            }
+        } else {
+            popup.style.backgroundColor = `rgba(0,0,0,.5)`
+            popup.style.display = 'block'
         }
     }
 
     const animationPopupClose = () => {
-        opacity = opacity - 0.02
-        posY = posY - 1
+        screenWidth = document.documentElement.clientWidth
+        console.log(screenWidth);
+        if (screenWidth >= 768) {
+            opacity = opacity - 0.05
+            posY = posY - 1
 
-        popup.style.backgroundColor = `rgba(0,0,0,${opacity})`;
-        popupWindow.style.transform = `translateY(${posY}%)`
-        if (opacity > 0) {
-            requestAnimationFrame(animationPopupClose)
+            popup.style.backgroundColor = `rgba(0,0,0,${opacity})`
+            popupWindow.style.transform = `translateY(${posY}%)`
+            if (opacity > 0) {
+                requestAnimationFrame(animationPopupClose)
+            } else {
+                popup.style.display = 'none'
+            }
         } else {
             popup.style.display = 'none'
         }
@@ -39,7 +56,6 @@ const modal = () => {
     })
 
     btnPopupClose.addEventListener('click', animationPopupClose)
-    popup.addEventListener('click', animationPopupClose)
 
 
 
