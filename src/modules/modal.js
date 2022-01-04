ca3981066e34f4b5cce05df81ad9bd447f26b10d
MikeDestroyer <Treil911@gmail.com>
2022-01-04 13:13:14 +0300

    update

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,15 +1,48 @@
 'use strict'
-
+let opacity = 0
+let posY = -5
 const modal = () => {
+
     const popup = document.querySelector('.popup')
+    const popupWindow = document.querySelector('.popup-content')
     const buttons = document.querySelectorAll('.popup-btn')
     const btnPopupClose = document.querySelector('.popup-close')
 
+    const animationPopupOpen = () => {
+        popup.style.display = 'block'
+        opacity = opacity + 0.02
+        posY = posY + 1
+
+        popup.style.backgroundColor = `rgba(0,0,0,${opacity})`
+        popupWindow.style.transform = `translateY(${posY}%)`
+        if (opacity < 0.5) {
+            requestAnimationFrame(animationPopupOpen)
+        }
+    }
+
+    const animationPopupClose = () => {
+        opacity = opacity - 0.02
+        posY = posY - 1
+
+        popup.style.backgroundColor = `rgba(0,0,0,${opacity})`;
+        popupWindow.style.transform = `translateY(${posY}%)`
+        if (opacity > 0) {
+            requestAnimationFrame(animationPopupClose)
+        } else {
+            popup.style.display = 'none'
+        }
+    }
+
+
     buttons.forEach((button) => {
-        button.addEventListener('click', () => { popup.style.display = 'block' })
+        button.addEventListener('click', animationPopupOpen)
     })
 
-    btnPopupClose.addEventListener('click', () => { popup.style.display = 'none' })
+    btnPopupClose.addEventListener('click', animationPopupClose)
+    popup.addEventListener('click', animationPopupClose)
+
+
+
 }
 
 export default modal
