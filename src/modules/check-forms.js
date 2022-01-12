4a78ca5b1da4f71daa469febc5f2509f142999eb
MikeDestroyer <Treil911@gmail.com>
2022-01-12 16:06:51 +0300

    made less vars

--- src/modules/check-forms.js
+++ src/modules/check-forms.js
@@ -1,16 +1,7 @@
 'use strict'
 const CheckForms = () => {
-    const formTopName = document.getElementById('form1-name')
-    const formTopEmail = document.getElementById('form1-email')
-    const formTopPhone = document.getElementById('form1-phone')
-
-    const formBottomName = document.getElementById('form2-name')
-    const formBottomEmail = document.getElementById('form2-email')
-    const formBottomPhone = document.getElementById('form2-phone')
-
-    const formModalName = document.getElementById('form2-name')
-    const formModalEmail = document.getElementById('form2-email')
-    const formModalPhone = document.getElementById('form2-phone')
+    const forms = document.querySelectorAll('form')
+    const calcInputs = document.querySelectorAll('input.calc-item')
 
     const calcType = document.querySelector('.calc-type')
     const calcSquare = document.querySelector('.calc-square')
@@ -18,9 +9,6 @@ const CheckForms = () => {
     const calcDay = document.querySelector('.calc-day')
 
 
-    const inputOnlyNumbers = (input) => {
-        input.target.value = input.target.value.replace(/\D+/g, "")
-    }
     const inputFormName = (input) => {
         input.target.value = input.target.value.replace(/[^а-я ]/gi, "")
     }
@@ -30,28 +18,28 @@ const CheckForms = () => {
     const inputFormPhone = (input) => {
         input.target.value = input.target.value.replace(/[^\d\(\)-]/g, "")
     }
+    const inputOnlyNumbers = (input) => {
+        input.target.value = input.target.value.replace(/\D+/g, "")
+    }
     const cleanCalcForm = () => {
         calcSquare.value = ''
         calcCount.value = ''
         calcDay.value = ''
     }
 
-    formTopName.addEventListener('input', inputFormName)
-    formTopEmail.addEventListener('input', inputFormEmail)
-    formTopPhone.addEventListener('input', inputFormPhone)
 
-    formBottomName.addEventListener('input', inputFormName)
-    formBottomEmail.addEventListener('input', inputFormEmail)
-    formBottomPhone.addEventListener('input', inputFormPhone)
-
-    formModalName.addEventListener('input', inputFormName)
-    formModalEmail.addEventListener('input', inputFormEmail)
-    formModalPhone.addEventListener('input', inputFormPhone)
-
-    calcSquare.addEventListener('input', inputOnlyNumbers)
-    calcCount.addEventListener('input', inputOnlyNumbers)
-    calcDay.addEventListener('input', inputOnlyNumbers)
+    forms.forEach((element) => {
+        const formName = element.querySelector('input[type=text]')
+        const formEmail = element.querySelector('input[type=email]')
+        const formPhone = element.querySelector('input[type=tel]')
+        formName.addEventListener('input', inputFormName)
+        formEmail.addEventListener('input', inputFormEmail)
+        formPhone.addEventListener('input', inputFormPhone)
+    })
+    calcInputs.forEach(element => {
+        element.addEventListener('input', inputOnlyNumbers)
+    })
     calcType.addEventListener('input', cleanCalcForm)
-
 }
+
 export default CheckForms
